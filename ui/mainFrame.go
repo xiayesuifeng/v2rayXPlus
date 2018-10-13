1d63085870473f370f3c0a0dd61937d7da10c38b
xiayesuifeng <gdigrgg@gmail.com>
2018-10-13 18:31:22 +0800

    add start button and v2ray version

--- ui/mainFrame.go
+++ ui/mainFrame.go
@@ -3,10 +3,15 @@ package ui
 import (
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
+	core2 "gitlab.com/xiayesuifeng/v2rayxplus/core"
 )
 
 type MainFrame struct {
 	*widgets.QFrame
+
+	vboxLayout *widgets.QVBoxLayout
+
+	startButton *widgets.QPushButton
 }
 
 func NewMainFrame(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *MainFrame {
@@ -19,9 +24,34 @@ func NewMainFrame(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *MainFrame
 }
 
 func (m *MainFrame) init() {
+	m.vboxLayout = widgets.NewQVBoxLayout2(m)
 
+	m.startButton = widgets.NewQPushButton2("启动",m)
+	m.startButton.SetFixedSize2(230,230)
+
+	m.vboxLayout.AddSpacing(60)
+	m.vboxLayout.AddWidget(m.startButton,0,core.Qt__AlignHCenter)
+	m.vboxLayout.AddSpacing(48)
+	var versionLabel *widgets.QLabel
+	if version,err := core2.GetVension();err!= nil {
+		versionLabel = widgets.NewQLabel2("v2ray版本: 未安装", m, 0)
+	} else {
+		versionLabel = widgets.NewQLabel2("v2ray版本: "+version,m,0)
+	}
+
+	m.vboxLayout.AddWidget(versionLabel,0,core.Qt__AlignHCenter)
+
+	m.vboxLayout.AddStretch(1)
+
+	m.SetLayout(m.vboxLayout)
 }
 
 func (m *MainFrame) initConnect() {
-
+	m.startButton.ConnectClicked(func(checked bool) {
+		if m.startButton.Text()=="启动" {
+			m.startButton.SetText("关闭")
+		} else if m.startButton.Text()=="关闭" {
+			m.startButton.SetText("启动")
+		}
+	})
 }
