074a6c8cc888b91fafb3575abc4f060b36e25a8d
xiayesuifeng <gdigrgg@gmail.com>
2018-10-23 21:30:13 +0800

    add init ConfigPath function

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"github.com/therecipe/qt/widgets"
+	"gitlab.com/xiayesuifeng/v2rayxplus/conf"
 	"gitlab.com/xiayesuifeng/v2rayxplus/ui"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -14,4 +18,29 @@ func main() {
 	ui.NewMainWindow().Show()
 
 	os.Exit(app.Exec())
+}
+
+func init() {
+	var err error
+	conf.ConfigPath, err = getConfPath()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	if _, err := os.Stat(conf.ConfigPath); err != nil {
+		if os.IsNotExist(err) {
+			if err := os.MkdirAll(conf.ConfigPath, 0755); err != nil {
+				log.Panicln("config dir create failure")
+			}
+		}
+	}
+}
+
+func getConfPath() (string, error) {
+	path := os.Getenv("HOME")
+	if path == "" {
+		return "", errors.New("get home failure")
+	}
+
+	return filepath.Join(path, ".config/V2rayXPlus"), nil
 }
