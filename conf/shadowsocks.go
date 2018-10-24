8c767b00bbcaa058cd3b62edd3d63ab31c56ec50
xiayesuifeng <gdigrgg@gmail.com>
2018-10-24 21:31:55 +0800

    add shadowsocks type

---
+++ conf/shadowsocks.go
@@ -0,0 +1,11 @@
+package conf
+
+type ShadowsocksServer struct {
+	Address  string `json:"address"`
+	Port     uint16 `json:"port"`
+	Cipher   string `json:"method"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Ota      bool   `json:"ota"`
+	Level    byte   `json:"level"`
+}
