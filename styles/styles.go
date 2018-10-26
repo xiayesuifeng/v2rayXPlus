9c30fe79f428e8ff3c8b0c1ae05199b83503a378
xiayesuifeng <gdigrgg@gmail.com>
2018-10-26 14:00:03 +0800

    add some styles

--- styles/styles.go
+++ styles/styles.go
@@ -43,3 +43,76 @@ QPushBUtton::pressed {
   border-image: url(":/resources/outline-close-24px.svg");
   background-color: rgba(255, 255, 255, 0.1);
 }`
+
+const SaveButton string = `
+QPushButton {
+  border: 2px;
+  border-radius: 22px;
+  margins: 10px;
+  outline: none;
+  color: #eaeaea;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+QPushButton {
+  background-image: url(":/resources/outline-done-24px.svg") 2 2 2 2 repeat repeat;
+  background-color: rgb(255, 255, 255);
+}
+
+QPushButton::hover {
+  background-image: url(":/resources/outline-done-24px.svg");
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+QPushBUtton::pressed {
+  border-image: url(":/resources/outline-done-24px.svg");
+  background-image: rgba(255, 255, 255, 0.5);
+}`
+
+const CancelButton string = `
+QPushButton {
+  border: 2px;
+  border-radius: 22px;
+  outline: none;
+  color: #eaeaea;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+QPushButton {
+  background-image: url(":/resources/outline-close-24px.svg");
+  background-color: rgb(255, 255, 255);
+}
+
+QPushButton::hover {
+  background-image: url(":/resources/outline-close-24px.svg");
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+QPushBUtton::pressed {
+  background-image: url(":/resources/outline-close-24px.svg");
+  background-color: rgba(255, 255, 255, 0.5);
+}`
+
+const ConfigListItem string = `
+QPushButton {
+  border: none;
+  outline: none;
+  color: #fffffff;
+  background-position: left;
+  background-repeat: no-repeat;
+  border-radius:5px;
+}
+
+QPushButton::checked {
+  background-image: url(":/resources/outline-done-24px.svg");
+}
+
+QPushButton::hover {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+QPushBUtton::pressed {
+  background-color: rgba(255, 255, 255, 0.3);
+}`
