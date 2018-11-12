c20daeae4d05cb04ff4ebf93853d2e571c53981c
xiayesuifeng <gdigrgg@gmail.com>
2018-11-12 20:12:18 +0800

    add NewV2rayConfig function

--- conf/v2ray.go
+++ conf/v2ray.go
@@ -70,3 +70,51 @@ func (v2ray *V2rayConfig) Save(conf string) error {
 
 	return json.NewEncoder(confJson).Encode(v2ray)
 }
+
+func NewV2rayConfig() *V2rayConfig {
+	v2ray := &V2rayConfig{}
+
+	port := uint(Conf.ListerPort)
+	dokodemoConfig := &InboundConfig{Port: &port}
+	dokodemoConfig.DomainOverride = &[]string{"tls", "http"}
+	dokodemoConfig.Protocol = "dokodemo-door"
+	dokodemoConfig.Settings = []byte(`{"network": "tcp,udp","followRedirect": true}`)
+
+	freedomConfig := OutboundConfig{}
+	freedomConfig.Protocol = "freedom"
+	freedomConfig.Tag = "direct"
+	freedomConfig.StreamSetting = &StreamConfig{SocketSettings: &SocketConfig{Mark: 255}}
+
+	fieldRule := []byte(`{"type":"field",
+	 "ip":[
+                 "0.0.0.0/8",
+                 "10.0.0.0/8",
+                 "100.64.0.0/10",
+                 "127.0.0.0/8",
+                 "169.254.0.0/16",
+                 "172.16.0.0/12",
+                 "192.0.0.0/24",
+                 "192.0.2.0/24",
+                 "192.168.0.0/16",
+                 "198.18.0.0/15",
+                 "198.51.100.0/24",
+                 "::1/128",
+                 "fc00::/7",
+                 "fe80::/10"
+              ],
+              "outboundTag":"direct"
+	}`)
+
+	chinaSitesRule := []byte(`{"type": "chinasites","outboundTag": "direct"}`)
+	chinaIpRule := []byte(`{"type": "chinaip","outboundTag": "direct"}`)
+
+	routerConfig := &RouterConfig{}
+	routerConfig.Settings = &RouterRulesConfig{DomainStrategy: "IPIfNonMatch", RuleList: []json.RawMessage{fieldRule, chinaSitesRule, chinaIpRule}}
+
+	v2ray.DNSConfig = &DnsConfig{Servers: Conf.DnsServers}
+	v2ray.InboundConfig = dokodemoConfig
+	v2ray.OutboundDetours = append(v2ray.OutboundDetours, freedomConfig)
+	v2ray.RouterConfig = routerConfig
+
+	return v2ray
+}
