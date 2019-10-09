d4b5dcbf230f8718f9fdec7bbf654ca163c54d99
xiayesuifeng <gdigrgg@gmail.com>
2019-10-10 02:27:01 +0800

    add domainSocketConfig widget

---
+++ ui/configEdit/streamConfig/domainSocketConfig.go
@@ -0,0 +1,33 @@
+package streamConfig
+
+import (
+	"github.com/therecipe/qt/widgets"
+)
+
+type DomainSocketConfig struct {
+	*widgets.QWidget
+
+	pathLineEdit *widgets.QLineEdit
+}
+
+func NewDomainSocketConfig(parent widgets.QWidget_ITF) *DomainSocketConfig {
+	widget := widgets.NewQWidget(parent, 0)
+
+	domainSocketConfig := &DomainSocketConfig{QWidget: widget}
+	domainSocketConfig.init()
+
+	return domainSocketConfig
+}
+
+func (ptr *DomainSocketConfig) init() {
+	formLayout := widgets.NewQFormLayout(ptr)
+	formLayout.SetContentsMargins(0, 0, 0, 0)
+
+	ptr.pathLineEdit = widgets.NewQLineEdit(ptr)
+
+	ptr.pathLineEdit.SetPlaceholderText("/path/to/ds/file")
+
+	formLayout.AddRow3("path", ptr.pathLineEdit)
+
+	ptr.SetLayout(formLayout)
+}
