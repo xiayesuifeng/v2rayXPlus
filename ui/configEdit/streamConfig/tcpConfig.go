7bb2fa7a8e18e41f5d0cb6ef743d368d3e5ca4e7
xiayesuifeng <gdigrgg@gmail.com>
2019-10-10 02:08:24 +0800

    add tcpConfig widget

---
+++ ui/configEdit/streamConfig/tcpConfig.go
@@ -0,0 +1,31 @@
+package streamConfig
+
+import (
+	"github.com/therecipe/qt/widgets"
+)
+
+type TcpConfig struct {
+	*widgets.QWidget
+
+	tcpSettingsJsonEdit *widgets.QPlainTextEdit
+}
+
+func NewTcpConfig(parent widgets.QWidget_ITF) *TcpConfig {
+	widget := widgets.NewQWidget(parent, 0)
+
+	tcpConfig := &TcpConfig{QWidget: widget}
+	tcpConfig.init()
+
+	return tcpConfig
+}
+
+func (ptr *TcpConfig) init() {
+	formLayout := widgets.NewQFormLayout(ptr)
+	formLayout.SetContentsMargins(0, 0, 0, 0)
+
+	ptr.tcpSettingsJsonEdit = widgets.NewQPlainTextEdit(ptr)
+
+	formLayout.AddRow3("tcpSettings", ptr.tcpSettingsJsonEdit)
+
+	ptr.SetLayout(formLayout)
+}
