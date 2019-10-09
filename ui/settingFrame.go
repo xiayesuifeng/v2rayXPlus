7e0cbe118c2b425bbe76e2ce630e32145b75cf65
xiayesuifeng <gdigrgg@gmail.com>
2019-10-09 15:47:38 +0800

    add listen port and dns server settings

--- ui/settingFrame.go
+++ ui/settingFrame.go
@@ -3,6 +3,9 @@ package ui
 import (
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
+	"gitlab.com/xiayesuifeng/v2rayxplus/conf"
+	"strconv"
+	"strings"
 )
 
 type SettingFrame struct {
@@ -20,4 +23,24 @@ func NewSettingFrame(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *Settin
 
 func (ptr *SettingFrame) init() {
 	ptr.SetWindowTitle("设置")
+
+	vboxLayout := widgets.NewQVBoxLayout2(ptr)
+
+	baseLabel := widgets.NewQLabel2("基本", ptr, 0)
+
+	scrollArea := widgets.NewQScrollArea(ptr)
+	scrollLayout := widgets.NewQFormLayout(scrollArea)
+
+	portEdit := widgets.NewQLineEdit2(strconv.FormatInt(int64(conf.Conf.ListerPort), 10), ptr)
+
+	dnsEdit := widgets.NewQTextEdit2(strings.Join(conf.Conf.DnsServers, ","), ptr)
+	dnsEdit.SetFixedHeight(64)
+
+	scrollLayout.AddRow3("监听端口:", portEdit)
+	scrollLayout.AddRow3("DNS服务器:", dnsEdit)
+
+	vboxLayout.AddWidget(baseLabel, 0, core.Qt__AlignLeft)
+	vboxLayout.AddWidget(scrollArea, 1, 0)
+
+	ptr.SetLayout(vboxLayout)
 }
